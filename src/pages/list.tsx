748faa108dcf7a4c613c0c9a79e3873232865109
Danh05341 <danh05341@gmail.com>
2023-12-28 13:58:54 +0700

    Update

--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,136 +1,4 @@
-// import { ArrowBackIcon } from '@chakra-ui/icons';
-// import {
-// 	Center,
-// 	Table,
-// 	Thead,
-// 	Tbody,
-// 	Tr,
-// 	Th,
-// 	Td,
-// 	TableContainer,
-// 	IconButton,
-// 	Box,
-// 	useColorModeValue,
-// 	Text,
-// 	Stack,
-// 	Flex,
-// 	Image,
-// } from '@chakra-ui/react';
-// import axios from 'axios';
-// import { useRouter } from 'next/router';
-// import { useEffect, useState } from 'react';
-
-// export default function List() {
-// 	const router = useRouter();
-// 	const [list, setList] = useState<any>();
-// 	const getHistories = async () => {
-// 		try {
-// 			await axios
-// 				.get('http://localhost:8000/plant/predict_list', {
-// 					params: { page: 2 },
-// 					headers: {
-// 						// 'Access-Control-Allow-Origin': '*',
-// 						'Content-Type': 'application/json',
-// 					},
-// 				})
-// 				.then(res => {
-// 					setList(res?.data);
-// 				});
-// 		} catch (error) {}
-// 	};
-
-// 	const dateRender = date => {
-// 		const newDate = new Date(date);
-// 		return (
-// 			newDate.getFullYear() +
-// 			'-' +
-// 			(newDate.getMonth() + 1) +
-// 			'-' +
-// 			newDate.getDate()
-// 		);
-// 	};
-
-// 	useEffect(() => {
-// 		getHistories();
-// 	}, []);
-// 	return (
-// 		<Center py={6}>
-// 			<Box
-// 				w={'full'}
-// 				margin={'50px'}
-// 				bg={useColorModeValue('white', 'gray.900')}
-// 				boxShadow={'2xl'}
-// 				rounded={'md'}
-// 				p={6}
-// 				overflow={'hidden'}
-// 				position={'relative'}
-// 			>
-// 				<Flex
-// 					alignItems={'center'}
-// 					justifyContent={'space-between'}
-// 					maxW={'50%'}
-// 				>
-// 					<IconButton
-// 						colorScheme="teal"
-// 						aria-label="Call Segun"
-// 						size="lg"
-// 						icon={<ArrowBackIcon color={'white'} />}
-// 						onClick={() => router.back()}
-// 						cursor={'pointer'}
-// 						color={'teal'}
-// 					/>
-// 					<Text
-// 						transform={'translateX(50%)'}
-// 						color="teal"
-// 						fontSize={32}
-// 						fontWeight={700}
-// 					>
-// 						Lịch sử chẩn đoán
-// 					</Text>
-// 				</Flex>
-// 				<Stack marginY={10}>
-// 					<TableContainer>
-// 						<Table variant="simple">
-// 							<Thead>
-// 								<Tr>
-// 									<Th>Tên tiếng Anh</Th>
-// 									<Th>Tên tiếng Việt</Th>
-// 									<Th>Ảnh</Th>
-
-// 									<Th>Mầm bệnh</Th>
-// 									<Th>Triệu chứng</Th>
-// 									<Th>Phương pháp điều trị bệnh</Th>
-// 									<Th>Create at</Th>
-// 								</Tr>
-// 							</Thead>
-// 							<Tbody>
-// 								{list?.map((item, index) => (
-// 									<Tr key={index}>
-// 										<Td>{item?.PredictResult?.NameDisease_ENG}</Td>
-// 										<Td>{item?.PredictResult?.NameDisease_VN} </Td>
-// 										<Td>
-// 											<Image w={200} src={item.Image} alt="icon" />
-// 										</Td>
-// 										<Td>{item?.PredictResult?.Pathogens}</Td>
-// 										<Td>{item?.PredictResult?.Symptom}</Td>
-// 										<Td>{item?.PredictResult?.Treatment}</Td>
-// 										<Td>{dateRender(item?.DateTime)}</Td>
-// 									</Tr>
-// 								))}
-// 							</Tbody>
-// 						</Table>
-// 					</TableContainer>
-// 				</Stack>
-// 			</Box>
-// 		</Center>
-// 	);
-// }
-
-import {
-	ArrowBackIcon,
-	ChevronLeftIcon,
-	ChevronRightIcon,
-} from '@chakra-ui/icons';
+import { ArrowBackIcon } from '@chakra-ui/icons';
 import {
 	Center,
 	Table,
@@ -147,8 +15,6 @@ import {
 	Stack,
 	Flex,
 	Image,
-	HStack,
-	Button,
 } from '@chakra-ui/react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
@@ -157,22 +23,18 @@ import { useEffect, useState } from 'react';
 export default function List() {
 	const router = useRouter();
 	const [list, setList] = useState<any>();
-	const [currentPage, setCurrentPage] = useState(1);
-	const [totalPages, setTotalPages] = useState(1);
-
-	const getHistories = async (page: number) => {
+	const getHistories = async () => {
 		try {
-			const response = await axios.get(
-				'http://localhost:8000/plant/predict_list',
-				{
-					params: { page },
+			await axios
+				.get('http://localhost:8000/plant/predict_list/', {
 					headers: {
+						// 'Access-Control-Allow-Origin': '*',
 						'Content-Type': 'application/json',
 					},
-				}
-			);
-			setList(response?.data);
-			setTotalPages(response?.data?.total_pages);
+				})
+				.then(res => {
+					setList(res?.data);
+				});
 		} catch (error) {}
 	};
 
@@ -187,15 +49,9 @@ export default function List() {
 		);
 	};
 
-	const handlePageChange = newPage => {
-		setCurrentPage(newPage);
-		getHistories(newPage);
-	};
-
 	useEffect(() => {
-		getHistories(currentPage);
-	}, [currentPage]);
-
+		getHistories();
+	}, []);
 	return (
 		<Center py={6}>
 			<Box
@@ -215,7 +71,7 @@ export default function List() {
 				>
 					<IconButton
 						colorScheme="teal"
-						aria-label="Go back"
+						aria-label="Call Segun"
 						size="lg"
 						icon={<ArrowBackIcon color={'white'} />}
 						onClick={() => router.back()}
@@ -231,7 +87,6 @@ export default function List() {
 						Lịch sử chẩn đoán
 					</Text>
 				</Flex>
-
 				<Stack marginY={10}>
 					<TableContainer>
 						<Table variant="simple">
@@ -240,6 +95,7 @@ export default function List() {
 									<Th>Tên tiếng Anh</Th>
 									<Th>Tên tiếng Việt</Th>
 									<Th>Ảnh</Th>
+
 									<Th>Mầm bệnh</Th>
 									<Th>Triệu chứng</Th>
 									<Th>Phương pháp điều trị bệnh</Th>
@@ -264,25 +120,6 @@ export default function List() {
 						</Table>
 					</TableContainer>
 				</Stack>
-
-				{/* Pagination */}
-				<HStack spacing={2} justify="center" mt={4}>
-					<Button
-						onClick={() => handlePageChange(currentPage - 1)}
-						isDisabled={currentPage === 1}
-						leftIcon={<ChevronLeftIcon />}
-					>
-						Previous
-					</Button>
-					<Text>{`Page ${currentPage} of ${totalPages}`}</Text>
-					<Button
-						onClick={() => handlePageChange(currentPage + 1)}
-						isDisabled={currentPage === totalPages}
-						rightIcon={<ChevronRightIcon />}
-					>
-						Next
-					</Button>
-				</HStack>
 			</Box>
 		</Center>
 	);
